66d0e65c229dfb0f91ae559c60d75238cf1e7ab9
Tobias Barth <4nduril@gmail.com>
2015-07-24 14:06:36 +0200

    Decoupled internals from grunt for testing

--- tasks/hexo.js
+++ tasks/hexo.js
@@ -8,8 +8,6 @@
 
 'use strict';
 
-var path = require('path');
-
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
@@ -20,59 +18,14 @@ module.exports = function(grunt) {
     var options = this.options({
       root: '/'
     });
-
-	// Find the Hexo module and load it.
-	var hexoRoot = '.' + options.root;
-	var hexoPath = hexoRoot + 'node_modules/hexo/lib/hexo/index.js';
-	grunt.log.writeln(hexoPath);
-	var Hexo;
-
-	if (grunt.file.exists(hexoPath)) {
-		Hexo = require(path.join(process.cwd(), hexoPath));
-	} else {
-		grunt.log.error('Hexo can not be found. Is it installed? Exiting …');
-		return false;
-	}
 	
-	// Initialize Hexo …
-	var hexo = new Hexo(path.join(process.cwd(), hexoRoot), {});
+	var hexo = require('./lib/hexo');
 
-	// Asynchronous
 	var done = this.async();
-	hexo.init().then(function() {
-		hexo.call('generate', {}).then(function() {
-			done(hexo.exit());
-		}).catch(function(err) {
-			grunt.log.writeln('Fehler');
-			done(hexo.exit(err));
-		});
-	});
 
-    // Iterate over all specified file groups.
-/*    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+	hexo(options.root, 'generate', done, {}, true);
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });*/
   });
+	
 
 };
---
+++ tasks/lib/hexo.js
@@ -0,0 +1,74 @@
+/*
+ * grunt-hexo
+ * https://github.com/4nduril/grunt-hexo
+ *
+ * Copyright (c) 2015 Tobias Barth
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(hexoRoot, cliCmd, callback, options, isGrunt, altHexo) {
+	
+	var path = require('path');
+	var fs = require('fs');
+
+	var modulePath = 'node_modules/hexo/lib/hexo/index.js';
+	var noFolderErrMsg = 'No Hexo folder given.';
+	var hexoNotFoundErrMsg = 'Hexo can not be found. Is it installed? Exiting …';
+	var grunt;
+
+	if (isGrunt === undefined) {
+		isGrunt = false;
+	} else if (isGrunt) {
+		grunt = require('grunt');
+	}
+	
+	if (typeof hexoRoot === 'string') {
+		hexoRoot = path.join(process.cwd(), hexoRoot);
+	} else {
+		if (isGrunt) {
+			grunt.log.error(noFolderErrMsg);
+			return false;
+		} else {
+			throw new Error(noFolderErrMsg);
+		}
+	}
+
+
+	if (options === undefined) {
+		options = {};
+	}
+
+	var hexoPath = path.join(
+		hexoRoot,
+		modulePath
+		);
+
+	// Open Hexo constructor for injection (useful for testing ...)
+	var Hexo;
+	if (altHexo) {
+		Hexo = altHexo;
+	} else if (fs.existsSync(hexoPath)) {
+		Hexo = require(hexoPath);
+	} else {
+		if (isGrunt) {
+			grunt.log.error(hexoNotFoundErrMsg);
+			return false;
+		} else {
+			throw new Error(hexoNotFoundErrMsg);
+		}
+	}
+
+	// Initialize Hexo …
+	var hexo = new Hexo(hexoRoot, {});
+
+	// Call the specified cli command then exit cleanly and call callback
+	hexo.init().then(function() {
+		hexo.call(cliCmd, options).then(function() {
+			callback(hexo.exit());
+		}).catch(function(err) {
+			callback(hexo.exit(err));
+		});
+	});
+};
